959b515657dcc7ee54e997af7ab25033f07eada7
TatianaDiaz4 <116126008+TatianaDiaz4@users.noreply.github.com>
2023-02-13 21:42:05 -0500

    Update initModels.js

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,19 +1,22 @@
-const Users = require('./users.models');
-const Conversations = require('./conversations.models');
-const Messages = require('./messages.models');
-const Participants = require('./participants.models');
+const Conversations = require("./conversations.models")
+const Messages = require("./messages.models")
+const Participants = require("./participants.models")
+const Users = require("./users.models")
 
 const initModels = () => {
-  
-  Users.hasMany(Participants)
-  Participants.belongsTo(Users);
+    
+    //? Users -> Participants
+    Users.hasMany(Participants)
+    Participants.belongsTo(Users)
 
-  Conversations.hasMany(Participants);
-  Participants.belongsTo(Conversations);
-  
+    //? Conversations -> Participants
+    Conversations.hasMany(Participants)
+    Participants.belongsTo(Conversations)
 
-  Participants.hasMany(Messages);
-  Messages.belongsTo(Participants);
-};
+    //? Participants -> Messages
+    Participants.hasMany(Messages)
+    Messages.belongsTo(Participants)
 
-module.exports = initModels;
+}
+
+module.exports = initModels
