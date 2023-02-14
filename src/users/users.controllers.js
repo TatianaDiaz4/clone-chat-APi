734177b279a963594641ffb2fc4508ce182e9de4
TatianaDiaz4 <116126008+TatianaDiaz4@users.noreply.github.com>
2023-02-13 21:39:35 -0500

    Update users.controllers.js

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -1,60 +1,57 @@
-const Users = require("../models/users.models");
-const { uuid } = require("uuid");
+const Users = require('../models/users.models')
+const uuid = require('uuid')
 
-const findAllUsers = async () => {
-  const data = await Users.findAll();
-
-  return data;
-};
+const findAllUser = async () => {
+    const data = await Users.findAll()
+    return data
+}
 
 const findUserById = async (id) => {
-  const data = await Users.findOne({
-    where: {
-      id: id,
-    },
-  });
-
-  return data;
-};
+    const data = await Users.findOne({
+        where: {
+            id: id
+        }
+    })
+    return data
+}
 
 const createNewUser = async (userObj) => {
-  const newUser = {
-    id: uuid.v4(),
-    firstName: userObj.firstName,
-    lastName: userObj.lastName,
-    email: userObj.email,
-    password: userObj.password,
-    profileImage: userObj.profileImage,
-    isActive: userObj.isActive,
-    phone: userObj.phone,
-  };
+    const newUser = {
+        id: uuid.v4(),
+        firstName : userObj.firstName,
+        lastName : userObj.lastName,
+        email: userObj.email,
+        password: userObj.password,
+        profileImage: userObj.profileImage,
+        phone : userObj.phone
+    }
+    const data = await Users.create(newUser)
+    return data
+}
 
-  const data = await Users.create(newUser);
-  return data;
-};
-
-const updateUser = async (id, UserObj) => {
-  const data = await Users.update(UserObj, {
-    where: {
-      id: id,
-    },
-  });
-  return data[0];
-};
+const updateUser = async (id, userObj) => {
+    //data === 1
+    const data = await Users.update(userObj,{
+        where: {
+            id: id
+        }
+    })
+    return data[0]
+}
 
 const deleteUser = async (id) => {
-  const data = await Users.destroy({
-    where: {
-      id: id,
-    },
-  });
-  return data;
-};
+    const data = await Users.destroy({
+        where: {
+            id: id
+        }
+    })
+    return data
+}
 
 module.exports = {
-  findAllUsers,
-  findUserById,
-  createNewUser,
-  updateUser,
-  deleteUser,
-};
+    findAllUser,
+    findUserById,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
